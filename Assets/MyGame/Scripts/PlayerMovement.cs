6a90179b29a2f922c841fe4e4f1819586cbdebfd
Dejan01 <dejan.klickovic01@htl-salzburg.ac.at>
2022-01-18 09:12:33 +0100

    scripted PlayerMovement and created candy Prefab

--- Assets/MyGame/Scripts/PlayerMovement.cs
+++ Assets/MyGame/Scripts/PlayerMovement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public GameObject player;
+    public float speedNegative = -0.1f;
+    public float speedPositive = 0.1f;
+
+  
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+
+    } 
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            var playerX = gameObject.transform.position.x;
+            player.transform.position = new Vector2(playerX + speedNegative, -3.52f);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            var playerX = gameObject.transform.position.x;
+            player.transform.position = new Vector2(playerX + speedPositive, -3.52f);
+
+
+        }
     }
 }
