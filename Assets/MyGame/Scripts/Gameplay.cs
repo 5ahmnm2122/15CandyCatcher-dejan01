fd8974670dbc2b593879b64267137dfa5e63d62f
Dejan01 <dejan.klickovic01@htl-salzburg.ac.at>
2022-01-19 21:23:38 +0100

    created Prefab for Candy Spawner

--- Assets/MyGame/Scripts/Gameplay.cs
+++ Assets/MyGame/Scripts/Gameplay.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Gameplay : MonoBehaviour
 {
+
+
+    public GameObject candyOriginal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnObject", 2, 1);
+       
     }
 
-    // Update is called once per frame
-    void Update()
+
+    /*void CloneCandys(int candyNumber){
+        for(int i = 0; i < candyNumber; i++)
+        {
+            GameObject CandyClone = Instantiate(candyCloneOriginal, Random.Range(30, 50));
+        }
+
+    }*/
+
+    Vector2 GetSpawnPoint()
     {
-        
+        float x = Random.Range(-7, 7);
+
+        return new Vector2(x, 8);
+
+    }
+
+    void SpawnObject()
+    {
+        Instantiate(candyOriginal, GetSpawnPoint(), Quaternion.identity); 
     }
 }
