72078fece977aeda31db03467c2a90e0140eec2c
Dejan01 <dejan.klickovic01@htl-salzburg.ac.at>
2022-01-21 10:10:39 +0100

    implemented new Player Movement Script with GetAxis

--- Assets/MyGame/Scripts/PlayerMovement.cs
+++ Assets/MyGame/Scripts/PlayerMovement.cs
@@ -6,36 +6,41 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public GameObject player;
-    public float speedNegative = -0.1f;
-    public float speedPositive = 0.1f;
+    //public float speedNegative = -0.1f;
+    public float speed = 10f;
+
+    Vector3 movement;
 
   
     // Start is called before the first frame update
     void Start()
     {
 
-    } 
-    
+    }
+
 
     // Update is called once per frame
     void Update()
     {
 
 
-       /* if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            var playerX = gameObject.transform.position.x;
-            player.transform.position = new Vector2(playerX + speedNegative, -3.52f);
-        }
+        /* if (Input.GetKey(KeyCode.LeftArrow))
+         {
+             var playerX = gameObject.transform.position.x;
+             player.transform.position = new Vector2(playerX + speedNegative, -3.52f);
+         }
 
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            var playerX = gameObject.transform.position.x;
-            player.transform.position = new Vector2(playerX + speedPositive, -3.52f);
-
-
-        }*/
+         if (Input.GetKey(KeyCode.RightArrow))
+         {
+             var playerX = gameObject.transform.position.x;
+             player.transform.position = new Vector2(playerX + speedPositive, -3.52f);
 
 
+         }*/
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        movement = new Vector3(horizontalInput, 0f, 0f);
+        transform.Translate(movement * speed * Time.deltaTime);
+        
     }
 }
